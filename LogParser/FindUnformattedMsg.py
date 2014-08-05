899a9e2f8dfc78feb310a39a52b4fddc6e3bdeee
Lirian Su <l9s@qad.com>
2014-08-05 14:25:15 +0800

    Complete the script function to find unformat msg

--- LogParser/FindUnformattedMsg.py
+++ LogParser/FindUnformattedMsg.py
@@ -15,7 +15,36 @@ def singleMsgs(logFile):
                         msg = msg + ls[j].strip() + '\n'
             l = l + [msg]
     return l
-
+	
+# Check one commit msg
+def checkMsg(msg):
+    msg = msg.strip()
+    def h(s,c,n):
+        if (len(s.split(c)) > n):
+            return 1
+        if (len(s.split(c)) == n):
+            return 0
+        if (len(s.split(c)) < n):
+            return -1
+    # Must contain 1orMore '-'
+    if (h(msg,'-',1)!=1):
+        return False
+    # Must contain 1orMore ': '
+    if (h(msg,': ',1)!=1):
+        return False
+    # There must be no more than 2 '\n\n'
+    if (h(msg,'\n\n',2)==1):
+        return False
+    # If has '\n\n' there must be no '\n' before it
+    if ((h(msg,'\n\n',1)!=0) and (h(msg.split('\n\n')[0],'\n',1) != 0)):
+        return False
+    # If has no '\n\n' there must be no '\n'
+    if ((h(msg,'\n\n',1)==0) and (h(msg,'\n',1) != 0)):
+        return False
+    # Better no more than 60 characters
+    # if len > 60 then False
+    return True
+	
 # Check subject
 def checkSub(msg):
     sub = msg.split('\n')[0]
@@ -27,7 +56,7 @@ l = singleMsgs('git.log')
 of = open('list.txt','w')
 n = 0
 for i in l:
-    if not checkSub(i):
+    if not checkSub(i) or not checkMsg(i):
         n += 1
         of.write(i)
         of.write('----------\n')
